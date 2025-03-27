0e2e55fffc2006f804411fe73c4714cf6b1fab11
tomichal <michalt48@gmail.com>
2025-03-27 13:07:30 -0700

    Update importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "google-maps", to: "https://maps.googleapis.com/maps/api/js?key=#{Rails.application.credentials.google_maps_api_key}&libraries=places"
+pin "google-maps", to: "https://maps.googleapis.com/maps/api/js?key=#{Rails.application.credentials.google_maps_api_key}&libraries=places&v=beta"
 
