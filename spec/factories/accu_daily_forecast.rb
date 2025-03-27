21bac23388997c89a309f13c02c1a779895b5ad5
tomichal <michalt48@gmail.com>
2025-03-27 12:21:17 -0700

    Add feature spec

--- spec/factories/accu_daily_forecast.rb
+++ spec/factories/accu_daily_forecast.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
-  factory :accu_condition do
+  factory :accu_daily_forecast do
     id { '2627449' }
-    starts_at { '2025-03-27T12:38:00-04:00' }
+    starts_at { '2025-03-27' }
     weather_text { 'Sunny' }
     weather_icon { 1 }
-    temperature { 43.0 }
+    temperature_min { 43.0 }
+    temperature_max { 53.0 }
     temperature_unit { 'F' }
     wind_speed { 13.7 }
     wind_unit { 'mi/h' }
--- spec/factories/accu_locations.rb
+++ spec/factories/accu_locations.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :accu_location do
     id { '2627449' }
+    primary_postal_code { '10007' }
+    country { 'US' }
+    latitude { 40.714 }
+    longitude { -74.003 }
   end
 end
